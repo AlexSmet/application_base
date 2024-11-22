f214d6a84011652a0e15991661d92acf476f60d8
Seednov 'Alex' Alexander <seednov.alexander@gmail.com>
2024-11-22 13:21:11 +0300

    Analysis fix, small upd

---
+++ lib/core/core.dart
@@ -0,0 +1,3 @@
+export 'package:application_base/core/const/navigator_transaction.dart';
+export 'package:application_base/core/service/logger_service.dart';
+export 'package:application_base/core/service/platform_service.dart';
