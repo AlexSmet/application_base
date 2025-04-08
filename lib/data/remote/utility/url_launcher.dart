49a6477399baa910a7efbfbf117e27956b96b36e
Seednov 'Alex' Alexander <seednov.alexander@gmail.com>
2025-04-08 22:06:24 +0300

    Url laucher added

---
+++ lib/data/remote/utility/url_launcher.dart
@@ -0,0 +1,51 @@
+import 'package:application_base/core/service/logger_service.dart';
+import 'package:url_launcher/url_launcher_string.dart';
+
+///
+abstract final class UrlLauncher {
+  /// Check and try to open a link.
+  /// Return **true** on success
+  static Future<bool> lauchLink(
+    String? link, {
+    LaunchMode mode = LaunchMode.externalApplication,
+  }) async {
+    if (link == null || link.isEmpty) return false;
+
+    try {
+      if (!await canLaunchUrlString(link)) {
+        logError(error: 'Can not launch the link $link');
+        return false;
+      }
+
+      if (await launchUrlString(link, mode: mode)) return true;
+
+      /// Maybe a problem with selected launch mode
+      if (mode != LaunchMode.inAppBrowserView) {
+        /// Try to open in app browser
+        logInfo(info: 'Error on launching the link $link via $mode');
+        if (await launchUrlString(link, mode: LaunchMode.inAppBrowserView)) {
+          return true;
+        }
+      }
+
+      logError(error: 'Error on launching the link $link');
+    } catch (error) {
+      logError(error: 'Error on launching the link $link: $error');
+    }
+    return false;
+  }
+
+  /// Try to open email application with prepeared email.
+  /// Return **true** on success
+  Future<bool> sendEmail({
+    required String to,
+    required String title,
+    required String body,
+  }) async {
+    /// Fix body - replace \n by %0D%0A and ' ' by %20
+    final String fixedBody =
+        body.replaceAll('\n', '%0D%0A').replaceAll(' ', '%20');
+    final String link = 'mailto:$to?subject=$title&body=$fixedBody';
+    return lauchLink(link);
+  }
+}
